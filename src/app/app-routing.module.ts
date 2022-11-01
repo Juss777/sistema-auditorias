67472965379697c992e7499b5f8099f116772c61
Luis Zapata <luis.azapatazepeda@gmail.com>
2022-11-01 16:56:21 -0600

    Cambios en lista de requerimiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,19 +14,19 @@ import { DetallePorAreaComponent } from "./components/detalle-por-area/detalle-p
 import { TableroComponent } from "./components/tablero/tablero.component";
 
 const routes: Routes = [
-  // {
-  //   path: "",
-  //   component: TableroComponent,
-  // },
   {
     path: "",
+    component: TableroComponent,
+  },
+  {
+    path: "dashboard",
     component: DashboardInicioComponent,
     data: {
       breadcrumb: [
-        // {
-        //   label: "Tablero de control",
-        //   url: "/",
-        // },
+        {
+          label: "Tablero de control",
+          url: "/",
+        },
         {
           label: "Dashboard",
           url: "/dashboard",
@@ -39,10 +39,10 @@ const routes: Routes = [
     component: DetalleAuditoriaComponent,
     data: {
       breadcrumb: [
-        // {
-        //   label: "Tablero de control",
-        //   url: "/",
-        // },
+        {
+          label: "Tablero de control",
+          url: "/",
+        },
         {
           label: "Dashboard",
           url: "/dashboard",
@@ -61,7 +61,7 @@ const routes: Routes = [
       breadcrumb: [
         {
           label: "Dashboard",
-          url: "/",
+          url: "/dashboard",
         },
         {
           label: "Ficha técnica",
@@ -81,7 +81,7 @@ const routes: Routes = [
       breadcrumb: [
         {
           label: "Dashboard",
-          url: "/",
+          url: "/dashboard",
         },
         {
           label: "HUB colaborativo",
@@ -97,7 +97,7 @@ const routes: Routes = [
       breadcrumb: [
         {
           label: "Dashboard",
-          url: "/",
+          url: "/dashboard",
         },
         {
           label: "HUB colaborativo",
