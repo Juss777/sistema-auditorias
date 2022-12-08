bf927c22362689ef8a946d4dafc400622dd6a33d
Luis Zapata <luis.azapatazepeda@gmail.com>
2022-12-08 16:26:32 -0600

    Cambio en filtros de buscador

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -9,6 +9,8 @@ export class BuscadorComponent implements OnInit {
   constructor() {}
   content = false;
   checkboxSelected: any[] = [];
+  selectedCountries!: any[];
+  filteredCountries!: any[];
   ngOnInit(): void {}
 
   cardResult = [
@@ -45,257 +47,93 @@ export class BuscadorComponent implements OnInit {
       options: [
         {
           id: 1,
-          label: "2022",
-        },
-        {
-          id: 2,
-          label: "2023",
-        },
-        {
-          id: 3,
-          label: "2024",
-        },
-        {
-          id: 4,
-          label: "2025",
-        },
-        {
-          id: 5,
-          label: "2026",
+          label: "2016",
         },
       ],
     },
     {
       id: 2,
-      name: "Contribuyente",
+      name: "Sociedad",
       options: [
         {
           id: 1,
-          label: "Contribuyente 1",
-        },
-        {
-          id: 2,
-          label: "Contribuyente 2",
-        },
-        {
-          id: 3,
-          label: "Contribuyente 3",
-        },
-        {
-          id: 4,
-          label: "Contribuyente 4",
-        },
-        {
-          id: 5,
-          label: "Contribuyente 5",
+          label: "Banco Azteca S.A. Institución de Banca Múltiple",
         },
       ],
     },
     {
       id: 3,
-      name: "Autoridad",
+      name: "Representante Legal",
       options: [
         {
           id: 1,
-          label: "Autoridad 1",
-        },
-        {
-          id: 2,
-          label: "Autoridad 2",
-        },
-        {
-          id: 3,
-          label: "Autoridad 3",
-        },
-        {
-          id: 4,
-          label: "Autoridad 4",
-        },
-        {
-          id: 5,
-          label: "Autoridad 5",
+          label: "Rafael Flores Birrichaga",
         },
       ],
     },
     {
       id: 4,
-      name: "Requerimiento",
+      name: "Autoridad",
       options: [
         {
           id: 1,
-          label: "Requerimiento 1",
-        },
-        {
-          id: 2,
-          label: "Requerimiento 2",
-        },
-        {
-          id: 3,
-          label: "Requerimiento 3",
-        },
-        {
-          id: 4,
-          label: "Requerimiento 4",
-        },
-        {
-          id: 5,
-          label: "Requerimiento 5",
+          label: "Servicio de Administración Tributaría",
         },
       ],
     },
     {
       id: 5,
-      name: "Observaciones",
+      name: "Etapas de proceso",
       options: [
         {
           id: 1,
-          label: "Observaciones 1",
+          label: "Requerimiento de información",
         },
         {
           id: 2,
-          label: "Observaciones 2",
-        },
-        {
-          id: 3,
-          label: "Observaciones 3",
-        },
-        {
-          id: 4,
-          label: "Observaciones 4",
-        },
-        {
-          id: 5,
-          label: "Observaciones 5",
+          label: "Oficio/Observaciones",
         },
       ],
     },
     {
       id: 6,
-      name: "Documentos presentados",
+      name: "Partida",
       options: [
         {
           id: 1,
-          label: "Documentos presentados 1",
+          label: "Generales",
         },
         {
           id: 2,
-          label: "Documentos presentados 2",
-        },
-        {
-          id: 3,
-          label: "Documentos presentados 3",
-        },
-        {
-          id: 4,
-          label: "Documentos presentados 4",
-        },
-        {
-          id: 5,
-          label: "Documentos presentados 5",
+          label: "Derivados",
         },
       ],
     },
     {
       id: 7,
-      name: "Etapas de proceso",
+      name: "Tipo de requerimiento",
       options: [
         {
           id: 1,
-          label: "Etapas de proceso 1",
+          label: "Documental/Probatorio",
         },
         {
           id: 2,
-          label: "Etapas de proceso 2",
-        },
-        {
-          id: 3,
-          label: "Etapas de proceso 3",
-        },
-        {
-          id: 4,
-          label: "Etapas de proceso 4",
-        },
-        {
-          id: 5,
-          label: "Etapas de proceso 5",
+          label: "Argumentativo/Descriptivo",
         },
       ],
     },
     {
       id: 8,
-      name: "Representante",
+      name: "Área Responsable",
       options: [
         {
           id: 1,
-          label: "Representante 1",
+          label: "Contralorí Banco Azteca",
         },
         {
           id: 2,
-          label: "Representante 2",
-        },
-        {
-          id: 3,
-          label: "Representante 3",
-        },
-        {
-          id: 4,
-          label: "Representante 4",
-        },
-        {
-          id: 5,
-          label: "Representante 5",
-        },
-      ],
-    },
-    {
-      id: 9,
-      name: "Área responsable",
-      options: [
-        {
-          id: 1,
-          label: "Área responsable 1",
-        },
-        {
-          id: 2,
-          label: "Área responsable 2",
-        },
-        {
-          id: 3,
-          label: "Área responsable 3",
-        },
-        {
-          id: 4,
-          label: "Área responsable 4",
-        },
-        {
-          id: 5,
-          label: "Área responsable 5",
-        },
-      ],
-    },
-    {
-      id: 11,
-      name: "Usuario",
-      options: [
-        {
-          id: 1,
-          label: "Usuario 1",
-        },
-        {
-          id: 2,
-          label: "Usuario 2",
-        },
-        {
-          id: 3,
-          label: "Usuario 3",
-        },
-        {
-          id: 4,
-          label: "Usuario 4",
-        },
-        {
-          id: 5,
-          label: "Usuario 5",
+          label: "Cobranza Ejecutiva",
         },
       ],
     },
@@ -311,6 +149,8 @@ export class BuscadorComponent implements OnInit {
 
     this.idMenuFilter = idMenu;
   }
+
+  filterCountry() {}
 
   // selectedOption(event: any) {
   //   console.log(event);
