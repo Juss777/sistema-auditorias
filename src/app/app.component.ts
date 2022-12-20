7b288d6d4101c211bb611fad5035b31b9a74df4c
Luis Zapata <luis.azapatazepeda@gmail.com>
2022-12-19 18:24:20 -0600

    se sustituyó el switch del translate del calendario chico a por un map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,100 +26,43 @@ export class AppComponent {
       .subscribe((res) => this.primengConfig.setTranslation(res));
   }
 
-  traslateCalendarDashBoardInit(){
+  traslateCalendarDashBoardInit(){   
     var collection: any = document.getElementsByClassName('p-datepicker-calendar ');
     var table: any = collection[0];
 
     var btnMonth: any = document.getElementsByClassName('p-datepicker-month');
-    
-    switch (btnMonth[0].innerText) {
-      case 'January':
-        btnMonth[0].innerHTML = 'Enero';
-        break;
-
-      case 'February':
-        btnMonth[0].innerHTML = 'Febrero';
-        break;
-
-      case 'March':
-        btnMonth[0].innerHTML = 'Marzo';
-        break;
-      
-      case 'April':
-        btnMonth[0].innerHTML = 'Abril';
-        break;
-    
-      case 'May':
-        btnMonth[0].innerHTML = 'Mayo';
-        break;
-
-      case 'June':
-        btnMonth[0].innerHTML = 'Junio';
-        break;
-
-      case 'July':
-        btnMonth[0].innerHTML = 'Julio';
-        break;
-
-      case 'August':
-        btnMonth[0].innerHTML = 'Agosto';
-        break;
-    
-      case 'September':
-        btnMonth[0].innerHTML = 'Septiembre';
-        break;
-  
-      case 'October':
-        btnMonth[0].innerHTML = 'Octubre';
-        break;
-  
-      case 'November':
-        btnMonth[0].innerHTML = 'Noviembre';
-        break;
-
-      case 'December':
-        btnMonth[0].innerHTML = 'Diciembre';
-        break;
-
-      default:
-        break;
+    const mapMonth: any = {
+      "January": "Enero",
+      "February": "Febrero",
+      "March": "Marzo",
+      "April": "Abril",
+      "May": "Mayo",
+      "June": "Junio",
+      "July": "Julio",
+      "August": "Agosto",
+      "September": "Septiembre",
+      "October": "Octubre",
+      "November": "Noviembre",
+      "December": "Diciembre",
     }
+
+    btnMonth[0].innerHTML = mapMonth[btnMonth[0].innerText];
 
     for (let i = 0; i < table.children[0].children[0].children.length; i++) {
       const element = table.children[0].children[0].children[i];
-      switch (element.children[0].innerHTML) {
-        case 'Wk':
-          element.children[0].innerHTML = 'Sem';
-          break;
 
-        case 'Su':
-          element.children[0].innerHTML = 'Do';
-          break;
+      const mapDay: any = {
+        "Wk": "Sem",
+        "Su": "Do",
+        "Mo": "Lu",
+        "Tu": "Ma",
+        "We": "Mi",
+        "Th": "Ju",
+        "Fr": "Vi",
+        "Sa": "Sa"
+      };
 
-        case 'Mo':
-          element.children[0].innerHTML = 'Lu';
-          break;
-        
-        case 'Tu':
-          element.children[0].innerHTML = 'Ma';
-          break;
-      
-        case 'We':
-          element.children[0].innerHTML = 'Mi';
-          break;
-
-        case 'Th':
-          element.children[0].innerHTML = 'Ju';
-          break;
-
-        case 'Fr':
-          element.children[0].innerHTML = 'Vi';
-          break;
-
-        default:
-          break;
-      }
-      
+      element.children[0].innerHTML = mapDay[element.children[0].innerHTML]      
     }
   }
 }
