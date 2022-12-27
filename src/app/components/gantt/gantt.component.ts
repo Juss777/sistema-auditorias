6d542a5df343cedde0e332a77a3de8a0d9cc7821
Luis Zapata <luis.azapatazepeda@gmail.com>
2022-12-27 09:18:10 -0600

    Gant

--- src/app/components/gantt/gantt.component.ts
+++ src/app/components/gantt/gantt.component.ts
@@ -1,35 +1,51 @@
-import { Component, OnInit, ViewChild, ElementRef, Input, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Input,
+  ViewEncapsulation,
+  AfterViewInit,
+} from "@angular/core";
 
-import { gantt } from 'dhtmlx-gantt';
-import { GanttServiceService } from '../../services/gantt-service.service';
-import { GanttData } from '../../class/auditoriasClass';
+// import { gantt } from "dhtmlx-gantt";
+import { gantt } from "dhtmlx-gantt";
+import { GanttServiceService } from "../../services/gantt-service.service";
+import { GanttData } from "../../class/auditoriasClass";
 
 @Component({
-  selector: 'app-gantt',
-  templateUrl: './gantt.component.html',
+  selector: "app-gantt",
+  templateUrl: "./gantt.component.html",
   styleUrls: [],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class GanttComponent implements OnInit {
-
-  @ViewChild("gantt", {static: true}) ganttContainer!: ElementRef;
+  @ViewChild("gantt", { static: true }) ganttContainer!: ElementRef;
   ganttData: GanttData[];
   @Input() columns: any[] = [
-    {name: "text", label: " ", width: 70, resize: true, min_width: 70}
+    { name: "text", label: " ", width: 70, resize: true, min_width: 70 },
   ];
-  @Input() periodGantt : string = "Mes";
-  @Input() year : number = 2023;
+  @Input() periodGantt: string = "Mes";
+  @Input() year: number = 2023;
 
-  constructor( private ganttService: GanttServiceService) {
+  constructor(private ganttService: GanttServiceService) {
     this.ganttData = ganttService.gantt.data;
   }
   ngOnInit(): void {
     this.loadGantt();
-    setTimeout(()=>{this.paintRow();this.paintRowTaskBar();this.paintTaskCellDashed()},700);
-    setInterval(() => {this.paintRow();this.paintRowTaskBar();this.paintTaskCellDashed()},100);
+    setTimeout(() => {
+      this.paintRow();
+      this.paintRowTaskBar();
+      this.paintTaskCellDashed();
+    }, 700);
+    setInterval(() => {
+      this.paintRow();
+      this.paintRowTaskBar();
+      this.paintTaskCellDashed();
+    }, 100);
   }
 
-  loadGantt(){
+  loadGantt() {
     gantt.clearAll();
     gantt.config.date_format = "%d-%m-%Y";
     gantt.init(this.ganttContainer.nativeElement);
@@ -45,77 +61,77 @@ export class GanttComponent implements OnInit {
       css: "gantt_container",
       cols: [
         {
-           width:100
-        }
-      ]
+          width: 100,
+        },
+      ],
     };
 
     switch (this.periodGantt) {
       case "Semana":
-        gantt.config.scales = [
-          {unit: "week", step: 1, format: "Semana %w"},
-        ];
+        gantt.config.scales = [{ unit: "week", step: 1, format: "Semana %w" }];
         break;
       case "Mes":
         gantt.config.scales = [
-          {unit: "month", step: 1, format: "<div class='gantt-scale-title'><div>%M</div><div class='gantt-grid-4'><div><span>Sem</span><span>1</span></div><div><span>Sem</span><span>2</span></div><div><span>Sem</span><span>3</span></div><div><span>Sem</span><span>4</span></div></div></div>"},
+          {
+            unit: "month",
+            step: 1,
+            format:
+              "<div class='gantt-scale-title'><div>%M</div><div class='gantt-grid-4'><div><span>Sem</span><span>1</span></div><div><span>Sem</span><span>2</span></div><div><span>Sem</span><span>3</span></div><div><span>Sem</span><span>4</span></div></div></div>",
+          },
         ];
         break;
       case "Año":
-        gantt.config.scales = [
-          {unit: "year", step: 1, format: "%Y"},
-        ];
+        gantt.config.scales = [{ unit: "year", step: 1, format: "%Y" }];
         break;
     }
 
-    gantt.templates.task_text=function(start,end,task){
-      return ""; 
-    }; 
+    gantt.templates.task_text = function (start, end, task) {
+      return "";
+    };
 
-    gantt.templates.task_class = function(start:any, end:any,task:any) {
+    gantt.templates.task_class = function (start: any, end: any, task: any) {
       let planned_end = new Date(task.planned_end);
       let final = new Date(end);
-      if(task.text=="Plan") {
+      if (task.text == "Plan") {
         return "gantt-plan";
       }
-      
-      
-      return "gantt-task";
-    }
 
-    gantt.parse({data: this.ganttData});
+      return "gantt-task";
+    };
+
+    gantt.parse({ data: this.ganttData });
   }
 
   paintRow() {
-    let divParent:HTMLCollection = document.getElementsByClassName("gantt_grid_data");
+    let divParent: HTMLCollection =
+      document.getElementsByClassName("gantt_grid_data");
     let divChild = divParent[0].children;
     let contentChildTask = [];
     for (let i = 0; i < divChild.length; i++) {
       const child = divChild[i];
-      if(child.classList[1] != "odd") {
+      if (child.classList[1] != "odd") {
         contentChildTask.push(child);
       }
-      
     }
-    contentChildTask.forEach((element,i) => {
-      if (i%2==1) {
+    contentChildTask.forEach((element, i) => {
+      if (i % 2 == 1) {
         element.classList.add("gantt-row-gray");
       }
-    })
-    
+    });
   }
 
   paintRowTaskBar() {
-    let divParent:HTMLCollection = document.getElementsByClassName("gantt_task_bg");
+    let divParent: HTMLCollection =
+      document.getElementsByClassName("gantt_task_bg");
     let divChild = divParent[0].children;
-    let lastPainted:boolean = true;
+    let lastPainted: boolean = true;
     for (let i = 0; i < divChild.length; i++) {
-      if (i%2==0) {
+      if (i % 2 == 0) {
         if (lastPainted) {
           lastPainted = false;
         } else {
           divChild[i].classList.add("gantt-row-gray");
-          divChild[i+1].classList.add("gantt-row-gray");
+          divChild[i + 1].classList.add("gantt-row-gray");
           lastPainted = true;
         }
       }
@@ -123,8 +139,9 @@ export class GanttComponent implements OnInit {
   }
 
   paintTaskCellDashed() {
-    let divCellTask:HTMLCollection = document.getElementsByClassName("gantt_task_cell");
-    for (let i = 0; i < divCellTask.length; i++)  {
+    let divCellTask: HTMLCollection =
+      document.getElementsByClassName("gantt_task_cell");
+    for (let i = 0; i < divCellTask.length; i++) {
       divCellTask[i].innerHTML = `
         <div class="gantt-week-taskcell-dashed">
           <div></div>
@@ -132,8 +149,7 @@ export class GanttComponent implements OnInit {
           <div></div>
           <div></div>
         </div>
-      `
+      `;
     }
   }
-
 }
